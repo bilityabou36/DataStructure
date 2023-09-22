7c72a779f0c9914c2ad014c34cb2ff68d4b6f758
abou bility <bilityabou36@gmail.com>
2023-09-21 23:34:42 -0500

    first commit

--- dataStructure/hello-type/src/index.ts
+++ dataStructure/hello-type/src/index.ts
@@ -1,2 +1,10 @@
 let sales: number = 123_345_455;
 
+function calculateTax(income: number, taxYear= 2022): number{
+    if (taxYear < 2022)
+       return income * 1.2
+    return income * 1.3
+
+}
+
+calculateTax(10_000)
