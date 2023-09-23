1d75fb71196f149d45a82e6ebd66950419de4b1e
abou bility <bilityabou36@gmail.com>
2023-09-22 19:56:21 -0500

    daily

--- dataStructure/hello-type/src/index.ts
+++ dataStructure/hello-type/src/index.ts
@@ -1,10 +1,34 @@
-let sales: number = 123_345_455;
+// String Types mini-challenge
+// Write a function that will display the most recent reviewers name next to the review total,
+// making sure to assign a type to the parameter, to prevent unwanted behaviour.
+// : string
 
-function calculateTax(income: number, taxYear= 2022): number{
-    if (taxYear < 2022)
-       return income * 1.2
-    return income * 1.3
+const reviewTotalDisplay = document.querySelector('#reviews')
 
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+// Solution
+function showReviewTotal (value : number, reviewer: string) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer
 }
 
-calculateTax(10_000)
+showReviewTotal(reviews.length, reviews[0].name)
